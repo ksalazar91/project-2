58ecc7e0916176fe8b59bd8276f2404ceb7596d7
Ksalazar91 <kalfaros91@gmail.com>
2018-09-09 17:55:08 -0600

    add to travis, fix models and creat the playerProfile.js

--- models/playerProfile.js
+++ models/playerProfile.js
@@ -1,30 +1,22 @@
-const bcrypt = require("bcrypt");
-
 module.exports = function(sequelize, DataTypes) {
-    var player = sequelize.define("Player", {
-        id: {
-            type: DataTypes.INTAGER, 
-            autoIncrement: ture,
-            primryKey: ture
-        },
-      name: DataTypes.STRING,
-      Age: DataTypes.INTAGER,
-      Gender: DataTypes.STRING,
-      username: DataTypes.STRING,
-      Species: DataTypes.STRING,
-      email: DataTypes.STRING,
-      pswd: DataTypes.STRING},
-      {
-        freezeTableName: true,
-        instanceMethods: {
-            generateHash(password) {
-                return bcrypt.hash(password, bcrypt.genSaltSync(8));
-            },
-            validPassword(password) {
-                return bcrypt.compare(password, this.password);
-            }
-        }
 
+    var Profile = sequelize.define("playerProfile", {
+
+        id:{
+            type: DataTypes.INTEGER, 
+            autoIncrement: true,
+            primaryKey: true
+
+        }, 
+        name: DataTypes.STRING,
+        Age: DataTypes.INTEGER,
+        Gender: DataTypes.STRING,
+        username: DataTypes.STRING,
+        Species: DataTypes.STRING,
+        email: DataTypes.STRING,
+        pswd: DataTypes.STRING
+      
     });
-    return player;
+    return Profile;
   };
+  
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,19 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Profile.findAll({}).then(function(players) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        profile: players
       });
     });
   });
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Profile.findOne({ where: { id: req.params.id } }).then(function(players) {
       res.render("example", {
-        example: dbExample
+        profile: players
       });
     });
   });
